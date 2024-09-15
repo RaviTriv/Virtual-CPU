57d0b4cecb845c23be9b6bf98ec17c3f830c8977
Ravi <trivediravi561@gmail.com>
2024-09-15 14:24:06 -0400

    add some insturctions

--- include/cpu.h
+++ include/cpu.h
@@ -1,3 +1,5 @@
+#include <vector>
+using namespace std;
 
 class CPU
 {
@@ -6,6 +8,7 @@ class CPU
 public:
   static const byte MAX = 255;
   CPU();
+  void runCPU();
   void resetCPU();
 
 private:
@@ -13,6 +16,12 @@ private:
   byte IR;
   byte register1;
   byte register2;
+  byte register3;
   bool overflow;
   bool underflow;
+  void load1(const vector<byte> &program);
+  void load2(const vector<byte> &program);
+  void add();
+  void sub();
+  void store1(vector<byte> &program);
 };
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,4 +1,74 @@
 #include <iostream>
-#include <vector>
 #include "../include/cpu.h"
 
+void CPU::resetCPU()
+{
+  PC = 0;
+  IR = 0;
+  register1 = 0;
+  register2 = 0;
+  register3 = 0;
+  overflow = false;
+  underflow = false;
+}
+
+CPU::CPU()
+{
+  resetCPU();
+}
+
+CPU::~CPU() {}
+
+void CPU::load1(const vector<byte> &program)
+{
+  register1 = program[PC];
+  PC++;
+}
+
+void CPU::load2(const vector<byte> &program)
+{
+  register2 = program[PC];
+  PC++;
+}
+
+void CPU::add()
+{
+  register3 = register1 + register2;
+  if (register3 > MAX)
+  {
+    overflow = true;
+    register3 = MAX;
+  }
+  register1 = register3;
+}
+
+void CPU::sub()
+{
+  register3 = register2 - register1;
+  if (register3 < 0)
+  {
+    underflow = true;
+    register3 = 0;
+  }
+  register1 = register3;
+}
+
+void CPU::store1(vector<byte> &program)
+{
+  program[PC] = register1;
+  PC++;
+}
+
+void CPU::runCPU()
+{
+  resetCPU();
+  while (PC < MAX)
+  {
+    switch (IR)
+    {
+      if (1)
+      {
+      }
+    }
+  }
+}
